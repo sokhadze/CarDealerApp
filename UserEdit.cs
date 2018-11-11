b49a314c541f5162aa601cb004548b15e29c6bb6
Unknown <lerisokhadze@gmail.com>
2018-11-11 15:16:28 +0400

    updated user edit

--- UserEdit.cs
+++ UserEdit.cs
@@ -12,10 +12,18 @@ namespace CarDealerApp
 {
     public partial class UserEdit : Form
     {
-        public UserEdit()
+        public UserEdit(int id, string fname, string lname, string phone)
         {
             InitializeComponent();
+            RowID = id;
+            FirstName = fname;
+            LastName = lname;
+            Phone = phone;
         }
+        int RowID;
+        string FirstName;
+        string LastName;
+        string Phone;
 
         private void UserEdit_Load(object sender, EventArgs e)
         {
